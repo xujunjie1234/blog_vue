78ee62656baac4551e70a6b882897924759378ed
xujunjie1234 <1936725526@qq.com>
2020-07-01 21:53:28 +0800

    url

--- src/network/request.js
+++ src/network/request.js
@@ -3,11 +3,11 @@ import axios from "axios"
 export function request(config, success, failure) {
     //1创建实例
     const instance = axios.create({
-        // baseURL: "http://127.0.0.1:5000/",
-        baseURL: "http://134.175.155.79:5000/",
+        baseURL: "http://127.0.0.1:5000/",
+        // baseURL: "xxxxx/",
         timeout: 5000
     })
-    
+
     //2、请求拦截  
     instance.interceptors.request.use(config => {
         return config
@@ -25,5 +25,5 @@ export function request(config, success, failure) {
     return instance(config)
 }
 
-// export const baseURL = "http://127.0.0.1:5000"
-export const baseURL = "http://134.175.155.79:5000"
+export const baseURL = "http://127.0.0.1:5000"
+// export const baseURL = "http://xxxxx"
